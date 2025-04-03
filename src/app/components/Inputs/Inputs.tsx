6382dfbe9bfc9ea14983f94dfa739d0da95e3ea3
HugoZAuad <hugozeymer@hotmail.com>
2025-04-03 09:05:06 -0300

    Fix: Ajustes final no globals, começado a mexer nos inputs para inserir a logica depois

--- src/app/components/Inputs/Inputs.tsx
+++ src/app/components/Inputs/Inputs.tsx
@@ -1,11 +1,19 @@
+import ButtonCal from "@/app/components/Buttons/ButtonCal";
+
 export default function Input() {
   return (
     <main>
       <div>
-        <h1>Para calcular o seu IMC informe o que é pedido abaixo</h1>
-        <input type="text" />
-        
-        <input type="text" />
+        <h1 className="ml-50 text-blue-100 font-bold text-2xl">
+          Para calcular o seu IMC informe o que é pedido abaixo
+        </h1>
+        <div className="ml-50">
+          <input placeholder="Digite o seu peso" type="text" />
+          <input placeholder="Digite a sua altura" type="text" />
+        </div>
+        <div>
+          <ButtonCal />
+        </div>
       </div>
     </main>
   );
